a49e9eca288dd440284e2c503bca0ab0fcf4b2b3
mykhailo.kriuchek <mykhailo.kriuchek@pm.bet>
2021-05-18 11:00:09 +0300

    new project
    wdio

---
+++ test/specs/home.spec.js
@@ -0,0 +1,15 @@
+import homePage from '../pages/home.page';
+
+describe('Home Page Suite', function() {
+  it('should have Load Delay section', function() {
+    // Using component to capture repeating html part
+    homePage
+        .open()
+        .verifyTextInPage('UI Test Automation');
+    expect(homePage.pageModals[3].title).toEqual('nnnn');
+    expect(homePage.pageModals[3].content).toEqual(
+        'Ensure that a test is capable of waiting for a page to load');
+
+    utilities.takeScreenshot('homepage');
+  });
+});
